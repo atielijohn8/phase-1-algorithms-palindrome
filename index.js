0ee6be8b1615d0c9a1ddaeed43196fe320db45c1
atielijohn8 <atielijohn4@gmail.com>
2025-06-29 22:23:18 +0300

    alogarithimic

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const lowerWord = word.toLowerCase();
+  return lowerWord === lowerWord.split('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+  FUNCTION isPalindrome(word):
+  lowerWord = CONVERT word TO LOWERCASE
+  reversedWord = REVERSE lowerWord
+  IF lowerWord EQUALS reversedWord:
+    RETURN true
+  ELSE:
+    RETURN false
 */
 
 /*
   Add written explanation of your solution here
+    First, we convert the word to lowercase to handle case insensitivity (though the test cases don't show this need, it's good practice).
+
+    We split the word into an array of characters using split('').
+
+    We reverse the array using reverse().
+
+    We join the reversed array back into a string using join('').
+
+    Finally, we compare the original word (lowercased) with the reversed word.
 */
 
 // You can run `node index.js` to view these console logs
